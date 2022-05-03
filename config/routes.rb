3bdc7ee47708da76132ad4d865fa0db0596820f9
Hamza <hamzatarar8@gmail.com>
2022-05-03 17:12:06 +0200

    Corrected linter errors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "general_shopping_list#index"
-  resources :users, do
+  root 'general_shopping_list#index'
+  resources :users do
     resources :general_shopping_lists, only: [:index]
-    resources :foods, only: [:index, :create, :new, :destroy]
-    resources :recipes, only: [:index, :show, :create, :new, :destroy]
+    resources :foods, only: %i[index create new destroy]
+    resources :recipes, only: %i[index show create new destroy]
     resources :public_recipes, only: [:index]
   end
 end
